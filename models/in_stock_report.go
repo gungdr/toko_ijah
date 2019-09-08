838adffe2225c7e6d05882acba73e7d21de35a10
Agung Dwi Riansah <agungdwiriansah@gmail.com>
2019-09-08 19:59:10 +0700

    feat(models) : add in stock report model

---
+++ models/in_stock_report.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+//InStockReport ..
+type InStockReport struct {
+	Time          time.Time `json:"time,omitempty" db:"time,omitempty"`
+	SKU           string    `json:"sku,omitempty" db:"sku,omitempty"`
+	ProductName   string    `json:"product_name,omitempty" db:"product_name,omitempty"`
+	OrderQty      int       `json:"order_qty,omitempty" db:"order_qty,omitempty"`
+	ReceiptQty    int       `json:"receipt_qty,omitempty" db:"receipt_qty,omitempty"`
+	OrderPrice    float32   `json:"order_price,omitempty" db:"order_price,omitempty"`
+	ReceiptNumber string    `json:"receipt_number,omitempty" db:"receipt_number,omitempty"`
+	Notes         string    `json:"notes,omitempty" db:"notes,omitempty"`
+}
