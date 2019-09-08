2b3e4ca301c099b4d72fb4c0b8819b5d11ff389c
Agung Dwi Riansah <agungdwiriansah@gmail.com>
2019-09-09 03:23:42 +0700

    chore(in_stock_report) : move report query into view

--- repository/implementation/sqlite/report.go
+++ repository/implementation/sqlite/report.go
@@ -163,54 +163,15 @@ func (repo *reportRepository) GetOutStockReport() ([]*models.OutStockReport, err
 
 func (repo *reportRepository) GetInStockReport() ([]*models.InStockReport, error) {
 	query := `
-				select
-				ro.date as "time",
-				prod.sku as sku,
-				prod.name as product_name,
-				po.order_qty as order_qty,
-				ro.qty as receipt_qty,
-				po.sum_price as order_price,
-				coalesce(ro.receipt_number, '') as receipt_number,
-				coalesce(STRFTIME('%Y/%m/%d',ro.date) ||' terima ' || ro.qty, '' )as notes
-			from
-				(
-				select
-					purchase_order.id,
-					purchase_order.date,
-					purchase_order_detail.product_id,
-					count(*) order_qty,
-					purchase_order_detail.price,
-					sum(purchase_order_detail.price) as sum_price
-				from
-					purchase_order
-				INNER join purchase_order_detail on
-					purchase_order.id = purchase_order_detail.purchase_order_id
-				group by
-					purchase_order.date,
-					purchase_order_detail.product_id,
-					purchase_order_detail.price ) po
-			inner join (
-				SELECT
-					receipt_order.purchase_order_id,
-					receipt_order.receipt_number,
-					receipt_order.date,
-					receipt_order_detail.product_id,
-					count(*) qty
-				from
-					receipt_order
-				INNER join receipt_order_detail on
-					receipt_order.id = receipt_order_detail.receipt_order_id
-				group by
-					receipt_order.receipt_number,
-					receipt_order.date,
-					receipt_order_detail.product_id,
-					receipt_order.purchase_order_id )ro on
-				ro.purchase_order_id = po.id
-				and ro.product_id = po.product_id
-
-			INNER JOIN vw_product prod
-			on prod.id = ro.product_id
-			and prod.id = po.product_id
+		select time,
+		sku,
+		product_name,	
+		order_qty,
+		receipt_qty,	
+		order_price,
+		receipt_number,
+		notes
+		from vw_in_stock_report
 	`
 	var report []*models.InStockReport
 	err := repo.db.Select(&report, query)
