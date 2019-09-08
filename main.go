d78611bb6328f5ba856b8f11eb94c366724b6339
Agung Dwi Riansah <agungdwiriansah@gmail.com>
2019-09-08 20:12:25 +0700

    feat(main) : add main file where endpoints declared

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	delivery "toko_ijah/delivery/http/implementation"
+	"toko_ijah/infrastructure"
+	repository "toko_ijah/repository/implementation/sqlite"
+	usecase "toko_ijah/usecase/implementation"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	prefix := os.Getenv("SKU_PREFIX")
+	driver := os.Getenv("DB_DRIVER")
+	connString := os.Getenv("DB_CONNECTION")
+	htptPort := os.Getenv("HTTP_PORT")
+	db := infrastructure.InitDB(driver, connString)
+	defer db.Close()
+
+	log.Println(prefix)
+	reportRepo := repository.NewReportRepository(db, prefix)
+	reportUseCase := usecase.NewReportUseCase(reportRepo)
+	reportDelivery := delivery.NewReportDelivery(reportUseCase)
+
+	r := gin.Default()
+	reportRoutes := r.Group("/report")
+	{
+		reportRoutes.GET("/in-stock", reportDelivery.GetInStockReport)
+	}
+	r.Run(fmt.Sprintf(":%s", htptPort))
+
+}
