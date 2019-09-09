d0c7138448a9860218dc43e3c010c50dad951266
Agung Dwi Riansah <agungdwiriansah@gmail.com>
2019-09-09 11:48:51 +0700

    chore(infrastructure) : add format constant

---
+++ infrastructure/format.go
@@ -0,0 +1,17 @@
+package infrastructure
+
+const (
+	//FormatCSV ..
+	FormatCSV = "csv"
+	//FormatJSON ..
+	FormatJSON = "json"
+)
+
+const (
+	//DateFormat ..
+	DateFormat = "2006-01-02"
+	//DateTimeFormat ..
+	DateTimeFormat = "2006-01-02 15:04:05"
+	//DateFormatFormal ..
+	DateFormatFormal = "02 January 2006"
+)
